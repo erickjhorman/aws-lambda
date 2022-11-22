6dd5451377469b594cdffcc6703478fd59fade5a
Erick Romero <Erick_Romero@epam.com>
2022-11-22 16:51:46 -0500

    feat: add simplerHandler class
    Description: Aws java core library and mockito dependecies were added to create a simple requesthandler function

--- src/test/java/dev/danvega/SimpleHandlerTest.java
+++ src/test/java/dev/danvega/SimpleHandlerTest.java
@@ -1,4 +1,47 @@
+package dev.danvega;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class SimpleHandlerTest {
-  
+
+    private SimpleHandler simpleHandler;
+
+    @Mock
+    Context context;
+
+    @Mock
+    LambdaLogger logger;
+
+    @BeforeEach
+    public void setup() {
+        when(context.getLogger()).thenReturn(logger); //returning the mock object
+
+        Mockito.doAnswer(call -> {
+            System.out.println((String) call.getArgument(0));
+            return null;
+        }).when(logger).log(anyString());
+
+        simpleHandler = new SimpleHandler();
+    }
+
+    @Test
+    @DisplayName("Should Return UppercaseOfInput")
+    void testHandleRequest() {
+        when(context.getFunctionName()).thenReturn("handleRequest");
+        Assertions.assertEquals("HELLO, WORLD!", simpleHandler.handleRequest("hello, world!", context));
+    }
 }
